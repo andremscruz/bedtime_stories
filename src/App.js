875eabfc63498201bcee975c38f08061f2c671a7
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2024-01-09 18:26:20 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,49 +3,44 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
   const [stories, setStories] = useState([])
-  const [contentShow, setContentShow] = useState(false)
+  const [visibleStories, setVisibleStories] = useState([])
 
   useEffect(() => {
-    storiesServices
-      .getAll()
-      .then(intialStories => setStories(intialStories))
-  },[])
+    storiesServices.getAll().then((initialStories) => setStories(initialStories))
+  }, [])
 
-  function handleClick(){
-    return(
-      setContentShow(prevShow => !prevShow)
-    )
+  function handleClick(index) {
+    setVisibleStories((prevVisibility) => {
+      const updatedVisibility = [...prevVisibility]
+      updatedVisibility[index] = !updatedVisibility[index]
+      return updatedVisibility
+    })
   }
 
-
-  return(
+  return (
     <>
-    {stories.map(story =>
-    <div>
-      <div className='button-container'>
-        <button onClick={handleClick}>
-        <h3>{story.title}</h3>
-        <h5>Tempo: {story.readTime} min</h5>
-        </button>
-      </div>
-      
-      {contentShow &&
-        <div>
-          <h3 className='title' >{story.title}</h3>
-          <h4 className='author' >Escrito por {story.author}</h4>
-          <h5 className='time'>Tempo de leitura: {story.readTime} minutos</h5>
-          <p className='content'>{story.content}</p>
+      {stories.map((story, index) => (
+        <div key={index}>
+          <div className='button-container'>
+            <button onClick={() => handleClick(index)}>
+              <h3>{story.title}</h3>
+              <h5>Tempo: {story.readTime} min</h5>
+            </button>
+          </div>
+
+          {visibleStories[index] && (
+            <div>
+              <h3 className='title'>{story.title}</h3>
+              <h4 className='author'>Escrito por {story.author}</h4>
+              <h5 className='time'>Tempo de leitura: {story.readTime} minutos</h5>
+              <p className='content'>{story.content}</p>
+            </div>
+          )}
         </div>
-        
-      }
-      
-    </div>
-    
-    )}
+      ))}
     </>
-  )
+  );
 }
 
 export default App
